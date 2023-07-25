f7e21ff8db4eaaba69aec59ce3ff639b90efe6fc
Susanta <inbox.susanta@gmail.com>
2023-07-25 21:20:48 +0530

    Created basic folder structure

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+const DashboardPage = () => {
   return (
     <Button variant="destructive" size="lg">
       Click Me
     </Button>
   );
-}
+};
+
+export default DashboardPage;
