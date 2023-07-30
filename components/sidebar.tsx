50fde016b01094e277add963c9b5b8870971ba3c
Susanta <inbox.susanta@gmail.com>
2023-07-30 17:49:20 +0530

    Added API Limit UI Counter

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,7 @@ import {
   Code,
   Settings,
 } from 'lucide-react';
+import { FreeCounter } from '@/components/free-counter';
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
@@ -62,7 +63,11 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
@@ -96,6 +101,8 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
