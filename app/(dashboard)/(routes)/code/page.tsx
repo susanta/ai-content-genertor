5cf84dc768c008729bf3e1e84e868eb9ea1f6580
Susanta <inbox.susanta@gmail.com>
2023-08-02 22:33:28 +0530

    Finish Pro modal functionality & added into individual content generation

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -20,8 +20,10 @@ import { cn } from '@/lib/utils';
 import { UserAvatar } from '@/components/user-avatar';
 import { BotAvatar } from '@/components/bot-avatar';
 import ReactMarkDown from 'react-markdown';
+import { useProModal } from '@/hooks/use-pro-modal';
 
 const CodePage = () => {
+  const proModal = useProModal();
   const router = useRouter();
   const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
 
@@ -51,8 +53,9 @@ const CodePage = () => {
 
       form.reset();
     } catch (error: any) {
-      // TODO: Open Pro Model
-      console.log(error);
+      if (error?.response?.status === 403) {
+        proModal.onOpen();
+      }
     } finally {
       router.refresh();
     }
--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -4,11 +4,57 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
+import { Badge } from '@/components/ui/badge';
+import {
+  Check,
+  Code,
+  ImageIcon,
+  MessageSquare,
+  Music,
+  VideoIcon,
+  Zap,
+} from 'lucide-react';
+import { cn } from '@/lib/utils';
 import { useProModal } from '@/hooks/use-pro-modal';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+
+const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    color: 'text-violet-500',
+    bgColor: 'bg-violet-500/10',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+  },
+];
 
 export const ProModal = () => {
   const proModal = useProModal();
@@ -19,13 +65,35 @@ export const ProModal = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-            Upgrade to AI Content Generator
+            <div className="flex items-center gap-x-2 font-bold py-1">
+              Upgrade to AI Content Generator
+              <Badge variant="premium" className="uppercase text-sm py-1">
+                Pro
+              </Badge>
+            </div>
           </DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+          <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+            {tools.map((tool) => (
+              <Card
+                key={tool.label}
+                className="p-3 border-r-black/5 flex items-center justify-between"
+              >
+                <div className="flex items-center gap-x-4">
+                  <div className={cn('p-2 w-fit rounded-md', tool.bgColor)}>
+                    <tool.icon className={cn('w-6 h-6', tool.color)} />
+                  </div>
+                  <div className="font-semibold text-sm">{tool.label}</div>
+                </div>
+                <Check className="text-primary w-5 h-5" />
+              </Card>
+            ))}
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter>
+          <Button size="lg" variant="premium" className="w-full">
+            Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
