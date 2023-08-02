8edd6bb16ad8056860d1838bec7cfce7980aa397
Susanta <inbox.susanta@gmail.com>
2023-08-02 21:08:48 +0530

    Connected pro modal to Shad cn dialog box

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,32 @@
 'use client';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { useProModal } from '@/hooks/use-pro-modal';
+
 export const ProModal = () => {
-  return <div>Pro Modal</div>;
+  const proModal = useProModal();
+
+  return (
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+      {/* <DialogTrigger>Open</DialogTrigger> */}
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            Upgrade to AI Content Generator
+          </DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
